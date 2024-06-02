0caf60fb391b0afdde414f4b433c003f8d213e38
Kyle Maclachlan <120307903+Kmac907@users.noreply.github.com>
2024-06-01 22:44:39 -0800

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -4,9 +4,9 @@ resource "aws_db_instance" "example" {
   engine               = "postgres"
   engine_version       = "12.4"
   instance_class       = "db.t2.micro"
-  name                 = "exampledb"
-  username             = "foo"
-  password             = "bar12345"
+  name                 = var.db_name
+  username             = var.db_user
+  password             = var.db_password
   parameter_group_name = "default.postgres12"
   skip_final_snapshot  = true
   publicly_accessible  = false
